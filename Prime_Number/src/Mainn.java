e2a0433ee09ea03fda8a6052f569d6dc281511f0
Fatih Ünal <74363444+Renov4ted@users.noreply.github.com>
2024-10-27 18:25:03 +0300

    Add files via upload

---
+++ Prime_Number/src/Mainn.java
@@ -0,0 +1,14 @@
+
+public class Mainn {
+    public static void main(String[] args) {
+        for (int i = 2; i < 100 && i > 0; i++){
+
+            if (i == 2 || i== 3 || i == 5 || i== 7){
+                System.out.println(i);
+
+            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+                System.out.println(i);
+            }
+        }
+        }
+    }
