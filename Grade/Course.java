3f43d3924332ea2c608a068beb14b1bd620be200
Fatih Ünal <74363444+Renov4ted@users.noreply.github.com>
2024-11-07 17:38:39 +0300

    Add files via upload

---
+++ Grade/Course.java
@@ -0,0 +1,28 @@
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    Course(String name, String code, String prefix){
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        int note = 0;
+    }
+
+
+    void addTeacher(Teacher teacher){
+        if (teacher.branch.equals(this.prefix)){
+            this.teacher = teacher;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println("Öğretmen ve Ders bölümleri uyuşmuyor!");
+        }
+        this.teacher = teacher;
+    }
+    void printTeacher(){
+        this.teacher.print();
+    }
+}
